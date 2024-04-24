3543dd44e524acb8d61601a107787bf34455bb2b
Kirill Velichko <cirill@live.ru>
2024-04-24 22:23:50 +0300

    Rewrite all

--- main.cpp
+++ main.cpp
@@ -1,117 +1,91 @@
-#include <iostream>
 #include <array>
-#include <vector>
 #include <functional>
+#include <iostream>
 #include <memory>
-template <class T>
-class LazyBase : public std::enable_shared_from_this<LazyBase<T>>
-{
+#include <vector>
+
+template <bool AgressiveCopyMode, class T, class... Args>
+auto GenerateFactory(Args &&...args) {
+  if constexpr (AgressiveCopyMode) {
+    return [... args = std::forward<Args>(
+                args)]() mutable -> std::unique_ptr<std::decay_t<T>> {
+      return std::make_unique<std::decay_t<T>>(std::move<Args>(args)...);
+    };
+  } else {
+    return [... args = (std::forward<Args>(
+                args))]() mutable -> std::unique_ptr<std::decay_t<T>> {
+      return std::make_unique<std::decay_t<T>>(std::forward<Args>(args)...);
+    };
+  }
+}
+template <class T> class SimpleLazy final {
 private:
-    static void Deleter(LazyBase<T> *instanse)
-    {
-        if (instanse->isInited)
-        {
-            T *data = reinterpret_cast<T *>(instanse->data.data());
-            std::cout << "deleted really\n";
-            data->~T();
-        }
-        else{
-            std::cout << "deleted pseudeo\n";
-        }
+  std::function<std::unique_ptr<std::decay_t<T>>()> factory;
+  std::shared_ptr<std::decay_t<T>> internal;
+  bool cracked;
+  void CreateInternal() {
+    try {
+      if (cracked) {
+        throw std::runtime_error("Cant create object. There was some error "
+                                 "while creating factory\n");
+      }
+      this->internal = this->factory();
+    } catch (std::exception &e) {
+      this->cracked = true;
+      throw std::runtime_error(
+          std::string("Cant create object using factory for lazy because:\n") +
+          e.what());
+    } catch (...) {
+      this->cracked = true;
+      throw;
     }
-    std::vector<char> data;
-    bool isInited;
-    template <class... Types>
-    std::function<void()> InitorGen(Types &&...initData)
-    {
-        constexpr bool isAllRval {(std::is_rvalue_reference_v<Types> && ...)};
-        if constexpr(isAllRval){
-            return [this, ... args = std::move<Types>(initData)]() mutable
-        {
-            this->data.resize(sizeof(T));
-            new (data.data()) T(std::forward<Types>(args)...);
-            this->isInited = true;
-        };
-        }
-        return [this, ... args = initData]() mutable
-        {
-            this->data.resize(sizeof(T));
-            new (data.data()) T(std::forward<Types>(args)...);
-            this->isInited = true;
-        };
-    }
-    std::function<void()> Initor;
-    template <class... Types>
-    LazyBase(Types &&...constructorArgs)
-    {
-        this->isInited = false;
-        this->Initor = InitorGen(std::forward<Types>(constructorArgs)...);
-    }
-    LazyBase(const LazyBase<T> &data)
-    {
-        this->data = data.data;
-        this->Initor = data.Initor;
-        this->isInited = data.isInited;
-    }
+  }
 
 public:
-    template <class... Types>
-    static std::shared_ptr<LazyBase<T>> Construct(Types &&...initData)
-    {
-        std::shared_ptr<LazyBase<T>> result(new LazyBase<T>(std::forward<Types>(initData)...), &LazyBase<T>::Deleter);
-        return result;
+  template <bool AgressiveCopyMode, class... Args>
+  static SimpleLazy Create(Args &&...args) {
+    SimpleLazy result;
+    result.cracked = false;
+    result.factory = GenerateFactory<AgressiveCopyMode, T, Args...>(
+        std::forward<Args>(args)...);
+    return result;
+  }
+  template <class... Args> SimpleLazy(Args &&...args) : cracked(false) {
+    try {
+      this->factory =
+          GenerateFactory<false, T, Args...>(std::forward<Args>(args)...);
+    } catch (std::exception &e) {
+      this->cracked = true;
+      throw std::runtime_error(
+          std::string("Cant create factory for lazy because:\n") + e.what());
+    } catch (...) {
+      this->cracked = true;
+      throw;
     }
-    bool IsCreated() const noexcept{
-        return this->isInited;
+  }
+  SimpleLazy(const SimpleLazy<T> &another) = default;
+  SimpleLazy &operator=(const SimpleLazy<T> &another) = default;
+  SimpleLazy(SimpleLazy<T> &&another) = default;
+  SimpleLazy &operator=(SimpleLazy &&another) = default;
+  [[nodiscard]] bool IsInited() const noexcept {
+    return this->internal != nullptr && !this->cracked;
+  }
+  [[nodiscard]] bool IsCracked() const noexcept { return this->cracked; }
+  T *operator->() {
+    if (this->cracked) {
+      throw std::runtime_error("Object dont created correctly\n");
     }
-    auto CreateCopy() const{
-        std::shared_ptr<LazyBase<T>> result(new LazyBase<T>(*this), &LazyBase<T>::Deleter);
-        return result;
+    if (this->factory != nullptr) {
+      this->internal = this->factory();
+      this->factory = nullptr;
     }
-    auto GetInstanse()
-    {
-        return this->shared_from_this();
-    }
-    T &Get()
-    {
-        if (!this->isInited)
-        {
-            this->Initor();
-            this->Initor = nullptr;
-            this->isInited = true;
-        }
-        T *data = reinterpret_cast<T *>(this->data.data());
-        return *data;
-    }
+    return this->internal.get();
+  }
 };
-// template <class T>
-// using Lazy = std::shared_ptr<LazyBase<T>>;
-int main(int, char **)
-{
-    {
-        std::vector<int> ar(4, 3);
-        auto data = LazyBase<std::vector<int>>::Construct(4, 3);
-        std::cout << data.use_count() << '\n';
-        auto anotherInst = data->GetInstanse();
-        std::cout << data.use_count() << '\n';
-        auto& cont = data->Get();
-        for(auto val: cont){
-            std::cout << val << " val ";
-        }
-        std::cout << '\n';
-    }
-    {
-       auto data= LazyBase<int>::Construct(7);
-       std::cout << data->Get() << '\n';
-    }
-    {
-        auto data = LazyBase<int>::Construct();
-        std::cout << data->Get() << '\n';
-        auto dataCopy = data->CreateCopy();
-        std::cout << data.use_count() << '\n';
-        std::cout << data->Get() << '\n';
-        std::cout << dataCopy.use_count() << '\n';
-        std::cout << dataCopy->Get() << '\n';
 
-    }
+int main(int argc, char *argv[]) {
+  int x = 7;
+  SimpleLazy<std::array<int, 7>> l1(std::array<int, 7>{1, 2, 6, 1, 3, 2, 1});
+  std::cout << l1->at(0) << "\n";
+  return 0;
 }
